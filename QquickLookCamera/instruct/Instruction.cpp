51424352c065ba5dfb6d463e6eda6054a72c082f
Echo-M <337074370@qq.com>
2016-05-10 11:17:32 +0800

    修改控制指令

---
+++ QquickLookCamera/instruct/Instruction.cpp
@@ -0,0 +1,486 @@
+#include "Instruction.h"
+#include <iostream>
+Instruction::Instruction(USHORT port_target, ULONG ip_target, USHORT port_local = htons(4567), ULONG ip_local = inet_addr("192.168.1.1")) :
+	cmdCounter(0),
+	maxSend(5),
+	maxWaitS(2),
+	maxWaitUS(0),
+	cmosId(NoCMOS)
+{
+	sock = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	ULONG ul = 1;
+	::ioctlsocket(sock, FIONBIO, (ULONG*)&ul);//设为非阻塞模式
+
+	//填写本地地址信息
+	addr_local.sin_family = AF_INET;
+	addr_local.sin_addr.S_un.S_addr = ip_local;
+	addr_local.sin_port = htons(port_local);
+
+	//填写远程地址信息
+	addr_target.sin_family = AF_INET;
+	addr_target.sin_addr.S_un.S_addr = ip_target;
+	addr_target.sin_port = htons(port_target);
+
+	//将套接字与对应地址绑定
+	if (::bind(sock, (LPSOCKADDR)&addr_local, sizeof(addr_local)) == SOCKET_ERROR)
+	{
+		std::cout << "绑定失败" << std::endl;
+		//return;
+	}
+}
+
+Instruction::~Instruction()
+{
+	::closesocket(sock);
+}
+
+bool Instruction::SetCmosId(CMOSID id)
+{
+	if (id == NoCMOS)
+		return false;
+	cmosId = id;
+	return true;
+}
+bool Instruction::buildCmdReg(CMD* _cmd, int _addr, int _data)
+{
+	if (cmosId == NoCMOS)
+		return false;
+	_cmd->synWord = htonl(0x03CCF0FF);
+	_cmd->dataLength = htonl(0x00000002);
+	_cmd->counter = htons(cmdCounter);
+	switch (cmosId)
+	{
+	case CMOSE:
+		_cmd->form = htons(0x0300);
+		break;
+	case CMOS1:
+		_cmd->form = htons(0x0400);
+		break;
+	case CMOS2:
+		_cmd->form = htons(0x0500);
+		break;
+	case CMOS3:
+		_cmd->form = htons(0x0600);
+		break;
+	default:
+		break;
+	}
+	_cmd->timeStamp = htons(0x0000);
+	_cmd->checkSum = htons(0x0000);
+	unsigned long long cmdData{ 0x8000000000000000 };
+	unsigned long long a = _addr*4294967296;
+	cmdData |= a;
+	cmdData |= _data;
+	_cmd->data = htonll(cmdData);
+	return true;
+}
+
+bool Instruction::sendCmdReg(int _addr, int _data)
+{
+	int alen = sizeof(addr_target);
+	fd_set fdsock;//套接字集合
+	struct timeval timeout;
+	CMD* cmd = new CMD; //发送和接收的指令
+	int count{ 0 }; //发送次数计数
+	timeout.tv_sec = maxWaitS; // 等待时间（s）
+	timeout.tv_usec = maxWaitUS; // 等待时间（μs）
+
+	while (1)
+	{
+		if (!buildCmdReg(cmd, _addr, _data))
+		{
+			return false;
+		}
+		::sendto(sock, (char*)cmd, sizeof(CMD), 0, (sockaddr*)&addr_target, sizeof(addr_target));
+		cmdCounter++;
+		FD_ZERO(&fdsock); // 先清空
+		FD_SET(sock, &fdsock);
+		int nret = ::select(0, &fdsock, NULL, NULL, &timeout);//select()返回值：有未决I/O的套接字句柄的个数
+		if (nret > 0)
+		{
+			if (FD_ISSET(sock, &fdsock))//若该套接字可读
+			{
+				int nrecv = ::recvfrom(sock, (char*)cmd, sizeof(CMD), 0, (LPSOCKADDR)&addr_target, &alen);
+				if (nrecv == 24)
+				//if (ntohl(cmd->synWord) == 0x05CCF0FF && ntohs(cmd->form) == 0x && ntohll(cmd->data) == 0x )
+				{
+					return true;
+				}
+			}
+		}
+		count++;
+		if (count >= maxSend)
+		{
+			return false;
+		}
+	}
+}
+
+bool Instruction::Initial()
+{
+	if (cmosId == NoCMOS)
+		return false;
+	if (!Start())
+		return false;
+	if (!Reset())
+		return false;
+	if (!PowerUp())
+		return false;
+	if (!EnableClockManagement1())
+		return false;
+	if (!EnableClockManagement2())
+		return false;
+	if (!RequiredRegisterUpload())
+		return false;
+	if (!SoftPowerUp())
+		return false;
+
+	return true;
+}
+
+bool Instruction::Run()
+{
+	if (cmosId == NoCMOS)
+		return false;
+	if (!EnableSequencer())
+		return false;
+
+	return true;
+}
+
+bool Instruction::Stop()
+{
+	if (cmosId == NoCMOS)
+		return false;
+	if (!DisableSequencer())
+		return false;
+	if (!SoftPowerDown())
+		return false;
+	if (!DisableClockManagement2())
+		return false;
+	if (!DisableClockManagement1())
+		return false;
+	if (!PowerOff())
+		return false;
+
+	return true;
+}
+
+bool Instruction::Start()
+{
+	CMD *cmd = new CMD;
+	cmd->synWord = htonl(0x03CCF0FF);
+	cmd->dataLength = htonl(0x00000002);
+	cmd->counter = htons(cmdCounter);
+	cmd->form = htons(0x0100);
+	cmd->timeStamp = htons(0x0000);
+	cmd->checkSum = htons(0x0000);
+	cmd->data = htonll(0x0000000100000000);
+
+	int alen = sizeof(addr_target);
+	fd_set fdsock;//套接字集合
+	struct timeval timeout;
+	int count{ 0 }; //发送次数计数
+	timeout.tv_sec = maxWaitS; // 等待时间（s）
+	timeout.tv_usec = maxWaitUS; // 等待时间（μs）
+	while (1)
+	{
+		::sendto(sock, (char*)cmd, sizeof(CMD), 0, (sockaddr*)&addr_target, sizeof(addr_target));
+		cmdCounter++;
+		FD_ZERO(&fdsock); // 先清空
+		FD_SET(sock, &fdsock);
+		int nret = ::select(0, &fdsock, NULL, NULL, &timeout);//select()返回值：有未决I/O的套接字句柄的个数
+		if (nret > 0)
+		{
+			if (FD_ISSET(sock, &fdsock))//若该套接字可读
+			{
+				int nrecv = ::recvfrom(sock, (char*)cmd, sizeof(CMD), 0, (LPSOCKADDR)&addr_target, &alen);
+				if (nrecv == 24)
+					//if (ntohl(cmd->synWord) == 0x05CCF0FF && ntohs(cmd->form) == 0x && ntohll(cmd->data) == 0x )
+				{
+					return true;
+				}
+			}
+		}
+		count++;
+		if (count >= maxSend)
+		{
+			return false;
+		}
+	}
+}
+
+bool Instruction::Reset()
+{
+	CMD *cmd = new CMD;
+	cmd->synWord = htonl(0x03CCF0FF);
+	cmd->dataLength = htonl(0x00000002);
+	cmd->counter = htons(cmdCounter);
+	cmd->form = htons(0x0100);
+	cmd->timeStamp = htons(0x0000);
+	cmd->checkSum = htons(0x0000);
+	cmd->data = htonll(0x0000000200000000);
+	
+	int alen = sizeof(addr_target);
+	fd_set fdsock;//套接字集合
+	struct timeval timeout;
+	int count{ 0 }; //发送次数计数
+	timeout.tv_sec = maxWaitS; // 等待时间（s）
+	timeout.tv_usec = maxWaitUS; // 等待时间（μs）
+	while (1)
+	{
+		::sendto(sock, (char*)cmd, sizeof(CMD), 0, (sockaddr*)&addr_target, sizeof(addr_target));
+		cmdCounter++;
+		FD_ZERO(&fdsock); // 先清空
+		FD_SET(sock, &fdsock);
+		int nret = ::select(0, &fdsock, NULL, NULL, &timeout);//select()返回值：有未决I/O的套接字句柄的个数
+		if (nret > 0)
+		{
+			if (FD_ISSET(sock, &fdsock))//若该套接字可读
+			{
+				int nrecv = ::recvfrom(sock, (char*)cmd, sizeof(CMD), 0, (LPSOCKADDR)&addr_target, &alen);
+				if (nrecv == 24)
+					//if (ntohl(cmd->synWord) == 0x05CCF0FF && ntohs(cmd->form) == 0x && ntohll(cmd->data) == 0x )
+				{
+					return true;
+				}
+			}
+		}
+		count++;
+		if (count >= maxSend)
+		{
+			return false;
+		}
+	}
+}
+
+bool Instruction::PowerUp()
+{
+	CMD *cmd = new CMD;
+	cmd->synWord = htonl(0x03CCF0FF);
+	cmd->dataLength = htonl(0x00000002);
+	cmd->counter = htons(cmdCounter);
+	cmd->form = htons(0x0200);
+	cmd->timeStamp = htons(0x0000);
+	cmd->checkSum = htons(0x0000);
+	cmd->data = htonll(0x0000000100000000);
+	
+	int alen = sizeof(addr_target);
+	fd_set fdsock;//套接字集合
+	struct timeval timeout;
+	int count{ 0 }; //发送次数计数
+	timeout.tv_sec = maxWaitS; // 等待时间（s）
+	timeout.tv_usec = maxWaitUS; // 等待时间（μs）
+	while (1)
+	{
+		::sendto(sock, (char*)cmd, sizeof(CMD), 0, (sockaddr*)&addr_target, sizeof(addr_target));
+		cmdCounter++;
+		FD_ZERO(&fdsock); // 先清空
+		FD_SET(sock, &fdsock);
+		int nret = ::select(0, &fdsock, NULL, NULL, &timeout);//select()返回值：有未决I/O的套接字句柄的个数
+		if (nret > 0)
+		{
+			if (FD_ISSET(sock, &fdsock))//若该套接字可读
+			{
+				int nrecv = ::recvfrom(sock, (char*)cmd, sizeof(CMD), 0, (LPSOCKADDR)&addr_target, &alen);
+				if (nrecv == 24)
+				//if (ntohl(cmd->synWord) == 0x05CCF0FF && ntohs(cmd->form) == 0x && ntohll(cmd->data) == 0x )
+				{
+					return true;
+				}
+			}
+		}
+		count++;
+		if (count >= maxSend)
+		{
+			return false;
+		}
+	}
+}
+
+bool Instruction::EnableClockManagement1()
+{
+	//Part1
+	if (cmosId == CMOSE)
+	{
+		if (!sendCmdReg(2, 0x0002))//Monochrome sensor parallel mode selection
+			return false;
+	}
+	else
+	{
+		if (!sendCmdReg(2, 0x0003))//Color sensor parallel mode selection
+			return false;
+	}
+	if (!sendCmdReg(32, 0x200C))//Configure clock management
+		return false;
+	if (!sendCmdReg(20, 0x0000))//Configure clock management
+		return false;
+	if (!sendCmdReg(16, 0x0007))//Configure PLL bypass mode
+		return false;
+
+	return true;
+}
+
+bool Instruction::EnableClockManagement2()
+{
+	//Part2
+	if (!sendCmdReg(9, 0x0000))//Release clock generator soft reset
+		return false;
+	if (!sendCmdReg(32, 0x200E))//Enable logic clock
+		return false;
+	if (!sendCmdReg(34, 0x0001))//Enable logic blocks
+		return false;
+
+	return true;
+}
+
+bool Instruction::RequiredRegisterUpload()
+{
+	if (!sendCmdReg(41, 0x085A))//
+		return false;
+	if (!sendCmdReg(129, 0x0<<13 | 0x0001))//129[13]-0x0
+		return false;
+	if (!sendCmdReg(65, 0x288B))//
+		return false;
+	if (!sendCmdReg(66, 0x53C5))//
+		return false;
+	if (!sendCmdReg(67, 0x0344))//
+		return false;
+	if (!sendCmdReg(68, 0x0085))//
+		return false;
+	if (!sendCmdReg(70, 0x4800))//
+		return false;
+	if (!sendCmdReg(128, 0x4710))//
+		return false;
+	if (!sendCmdReg(197, 0x0103))//
+		return false;
+	if (!sendCmdReg(176, 0x00F5))//
+		return false;
+	if (!sendCmdReg(180, 0x00FD))//
+		return false;
+	if (!sendCmdReg(181, 0x0144))//
+		return false;
+
+	return true;
+}
+
+
+bool Instruction::SoftPowerUp()
+{
+	if (!sendCmdReg(32, 0x200F))//
+		return false;
+	if (!sendCmdReg(10, 0x0000))//
+		return false;
+	if (!sendCmdReg(64, 0x0001))//
+		return false;
+	if (!sendCmdReg(72, 0x0203))//
+		return false;
+	if (!sendCmdReg(40, 0x0003))//
+		return false;
+	if (!sendCmdReg(48, 0x0001))//
+		return false;
+	if (!sendCmdReg(112, 0x0000))//
+		return false;
+
+	return true;
+}
+bool Instruction::EnableSequencer()
+{
+	if (!sendCmdReg(192, 0x1<<0))//192[0]-0x1
+		return false;
+
+	return true;
+}
+bool Instruction::DisableSequencer()
+{
+	if (!sendCmdReg(192, 0x0 << 0))//192[0]-0x1
+		return false;
+
+	return true;
+}
+bool Instruction::SoftPowerDown()
+{
+	if (!sendCmdReg(112, 0x0000))//
+		return false; 
+	if (!sendCmdReg(48, 0x0000))//
+		return false;
+	if (!sendCmdReg(40, 0x0000))//
+		return false;
+	if (!sendCmdReg(72, 0x0200))//
+		return false;
+	if (!sendCmdReg(64, 0x0000))//
+		return false;
+	if (!sendCmdReg(10, 0x0999))//
+		return false;
+
+	return true;
+}
+bool Instruction::DisableClockManagement2()
+{
+	//Part2
+	if (!sendCmdReg(34, 0x0000))//Disable logic blocks
+		return false;
+	if (!sendCmdReg(32, 0x200C))//Disable logic clock
+		return false;
+	if (!sendCmdReg(9, 0x0009))//Soft reset clock generator
+		return false;
+
+	return true;
+}
+bool Instruction::DisableClockManagement1()
+{
+	//Part1
+	if (!sendCmdReg(16, 0x0000))//Disable PLL
+		return false;
+	if (!sendCmdReg(8, 0x0099))//Soft reset PLL
+		return false;
+	if (!sendCmdReg(20, 0x0000))//Configure clock management
+		return false;
+
+	return true;
+}
+
+bool Instruction::PowerOff()
+{
+	CMD *cmd = new CMD;
+	cmd->synWord = htonl(0x03CCF0FF);
+	cmd->dataLength = htonl(0x00000002);
+	cmd->counter = htons(cmdCounter);
+	cmd->form = htons(0x0200);
+	cmd->timeStamp = htons(0x0000);
+	cmd->checkSum = htons(0x0000);
+	cmd->data = htonll(0x0000000200000000);
+
+	int alen = sizeof(addr_target);
+	fd_set fdsock;//套接字集合
+	struct timeval timeout;
+	int count{ 0 }; //发送次数计数
+	timeout.tv_sec = maxWaitS; // 等待时间（s）
+	timeout.tv_usec = maxWaitUS; // 等待时间（μs）
+	while (1)
+	{
+		::sendto(sock, (char*)cmd, sizeof(CMD), 0, (sockaddr*)&addr_target, sizeof(addr_target));
+		cmdCounter++;
+		FD_ZERO(&fdsock); // 先清空
+		FD_SET(sock, &fdsock);
+		int nret = ::select(0, &fdsock, NULL, NULL, &timeout);//select()返回值：有未决I/O的套接字句柄的个数
+		if (nret > 0)
+		{
+			if (FD_ISSET(sock, &fdsock))//若该套接字可读
+			{
+				int nrecv = ::recvfrom(sock, (char*)cmd, sizeof(CMD), 0, (LPSOCKADDR)&addr_target, &alen);
+				if (nrecv == 24)
+					//if (ntohl(cmd->synWord) == 0x05CCF0FF && ntohs(cmd->form) == 0x && ntohll(cmd->data) == 0x )
+				{
+					return true;
+				}
+			}
+		}
+		count++;
+		if (count >= maxSend)
+		{
+			return false;
+		}
+	}
+}
+
---
+++ QquickLookCamera/instruct/Instruction.h
@@ -0,0 +1,69 @@
+#pragma once
+#include ".\\QData\\initsock.h"
+
+#pragma pack (push)
+#pragma pack (1)
+struct CMD
+{
+	//16 byte + 8 byte
+	unsigned int synWord{ 0 }; // 命令同步字 - 4 byte
+	unsigned int dataLength{ 0 }; // 命令数据长度 - 4 byte
+	unsigned short counter{ 0 }; // 命令计数器 - 2 byte
+	unsigned short form{ 0 }; // 命令类型
+	unsigned short timeStamp{ 0 }; // 时间戳
+	unsigned short checkSum{ 0 }; // 校验和
+
+	unsigned long long data{ 0 }; // 命令数据
+};
+
+#pragma pack (pop)
+
+//PC->FPGA的指令，控制相机参数改变 ；但是先要要个指令触发硬件发数据，然后InputCOMS开始接收数据。
+class Instruction
+{
+public:
+	enum CMOSID{ NoCMOS, CMOSE, CMOS1, CMOS2, CMOS3 };
+public:
+	Instruction(USHORT port_target, ULONG ip_target, USHORT port_local, ULONG ip_local);
+	~Instruction();
+
+	//设置CMOS编号，在调用发送指令的函数前应设置本函数
+	bool SetCmosId(CMOSID id);
+	bool Initial();
+	bool Run();
+	bool Stop();
+
+protected:
+	bool Start();
+	bool Reset();
+	bool PowerUp();
+	bool EnableClockManagement1();
+	bool EnableClockManagement2();
+	bool RequiredRegisterUpload();
+	bool SoftPowerUp();
+	bool EnableSequencer();
+
+	bool DisableSequencer();
+	bool SoftPowerDown();
+	bool DisableClockManagement2();
+	bool DisableClockManagement1();
+	bool PowerOff();
+	
+protected:
+	bool buildCmdReg(CMD* _cmd, int _addr, int _data);
+	bool sendCmdReg(int _addr, int _data);
+
+protected:
+	CInitSock c;//Winsock库的装入和释放
+	SOCKET sock;//接收图像数据套接字
+	sockaddr_in addr_local;//本地地址
+	sockaddr_in addr_target;//远程地址
+
+
+private:
+	CMOSID cmosId;
+	unsigned short cmdCounter;//初值为0
+	const int maxSend; //允许的最大发送次数
+	const int maxWaitS; //允许的最长等待时间s
+	const int maxWaitUS; //允许的最长等待时间us
+};
